5f540a9592db82781d22617e20f68906fad1ad4d
cyberplanner <saleh.gfx@gmail.com>
2016-06-23 21:30:43 +0100

    refactored directory.rb : converted students array to an array of hashes

--- directory.rb
+++ directory.rb
@@ -1,31 +1,33 @@
 # first we print the list of students
-students = [ "Dr. Hannibal Lecter",
-           "Darth Vader",
-           "Nurse Ratched",
-           "Michael Corleone",
-           "Alex DeLarge",
-           "The Wicked Witch of the West",
-           "Terminator",
-           "Freddy Krueger",
-           "The Joker",
-           "Joffrey Baratheon",
-           "Norman Bates"]
+students = [
+            {name: "Dr. Hannibal Lecter", cohort: :november},
+            {name: "Darth Vader", cohort: :november},
+            {name: "Nurse Ratched", cohort: :november},
+            {name: "Michael Corleone", cohort: :november},
+            {name: "Alex DeLarge", cohort: :november},
+            {name: "The Wicked Witch of the West", cohort: :november},
+            {name: "Terminator", cohort: :november},
+            {name: "Freddy Krueger", cohort: :november},
+            {name: "The Joker", cohort: :november},
+            {name: "Joffrey Baratheon", cohort: :november},
+            {name: "Norman Bates", cohort: :november}
+            ]
            
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-#Print each student in the array by iterating over the array's element
-def print names
-  names.each do |name|
-  puts name
+#Print each element in the array(array containing the name in [0] and the cohort in [1]) by iterating over the array
+def print students
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 #finally, we print the total number of students
-def print_footer names
-  puts "Overall, we have #{names.count} great students"
+def print_footer students
+  puts "Overall, we have #{students.count} great students"
 end
 
 #callign methods
