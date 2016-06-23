d653bb41ab8b35c99bc7d2f5e9337ab278d1cd5d
cyberplanner <saleh.gfx@gmail.com>
2016-06-23 23:22:19 +0100

    Updated directory.rb to include user input method, added .gitignore file

--- directory.rb
+++ directory.rb
@@ -1,18 +1,23 @@
-# first we print the list of students
-students = [
-            {name: "Dr. Hannibal Lecter", cohort: :november},
-            {name: "Darth Vader", cohort: :november},
-            {name: "Nurse Ratched", cohort: :november},
-            {name: "Michael Corleone", cohort: :november},
-            {name: "Alex DeLarge", cohort: :november},
-            {name: "The Wicked Witch of the West", cohort: :november},
-            {name: "Terminator", cohort: :november},
-            {name: "Freddy Krueger", cohort: :november},
-            {name: "The Joker", cohort: :november},
-            {name: "Joffrey Baratheon", cohort: :november},
-            {name: "Norman Bates", cohort: :november}
-            ]
-           
+# Adding a method for user ineraction/user input
+def input_students
+  puts "Please enter the names of the students:"
+  puts "To shinish, just hit return twice"
+  # the array in which the input is gonna be saved
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not 'empty?', repeat this code
+  while !name.empty? do
+    # Add the student hash to the array with a cohort hash
+    students << {name: name, cohort: :november}
+    # then puts the number of students/entries in the students array 
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -30,7 +35,9 @@ def print_footer students
   puts "Overall, we have #{students.count} great students"
 end
 
-#callign methods
+#calling methods
+#getting input from the user
+students = input_students
 print_header
 print students
 print_footer students
