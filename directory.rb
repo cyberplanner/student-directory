4cddcb0b2afec3eb75157cfc4b469c611d01262c
cyberplanner <saleh.gfx@gmail.com>
2016-06-25 23:13:44 +0100

    Added a method to save_students, added the option to save students to the interactive_menu

--- directory.rb
+++ directory.rb
@@ -130,6 +130,19 @@ def print_footer
   end
   puts # spacer line
 end
+#######################
+# save the students data to a csv file
+def save_students
+  file = File.open "students.csv", "w"
+  #iterating over the students array
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close  
+end
+
 ########################
 # Interactive menu
 ########################
@@ -143,6 +156,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the students"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
